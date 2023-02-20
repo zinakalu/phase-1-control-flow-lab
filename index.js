08680d1932fcca4b1343a38a81b3240a05dd9ec2
zinakalu <zina.kalu@gmail.com>
2023-02-20 13:43:13 -0500

    created control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet) {
+  if (distanceInFeet <= 400) {
+    return "This one is on me!";
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (distanceInFeet > 2500) {
+    return "No can do.";
+  }
+
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === 'NYC' ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
